5a47f08805a7ef48320d48c380d810d154c73d1e
joel-becker <joel_becker@mail.com>
2019-10-25 08:09:11 -0400

    Restrict wrangle_data to wrangling functions

--- wrangle_data.R
+++ wrangle_data.R
@@ -1,10 +1,9 @@
 #----------------------------------------------------------------------------------#
-# Plots gym data
+# Wrangles gym data
 # Author: Joel Becker
 
 # Notes:
-#   Takes google sheet containing 'date', 'exercise', 'weight' and 'sets'
-#   Assume constant reps, give some formulas of progress and visualise
+#   
 #----------------------------------------------------------------------------------#
 
 
@@ -168,26 +167,6 @@ wrangle.time.chart <- function(data = data) {
   return(data)
 }
 
-plot.time.chart <- function(data = data, location = F) {
-  # plots chart of decaying exercise intensity over time
-
-  if (location == TRUE) {
-    plot <- ggplot(data, aes(x=date, y=decay_intensity_bydatelocation)) +
-      geom_line(stat="smooth", method = "lm", se=FALSE, aes(group=month_number), colour="#7570b3", size=1.2, alpha=0.3) +
-      geom_line(size=1.2, colour="#7570b3")
-  } else {
-    plot <- ggplot(data, aes(x=date, y=decay_intensity_bydate)) +
-      geom_line(stat="smooth", method = "lm", se=FALSE, colour="#1b9e77", size=1.2, alpha=0.4) +
-      geom_line(size=1.2, colour="#7570b3")
-  }
-
-  plot <- plot +
-    theme_minimal() +
-    ylim(0, NA)
-
-  return(plot)
-}
-
 wrangle.workout.chart <- function(data = data) {
   # wrangles data in preperation for workout intensity chart
 
@@ -208,33 +187,5 @@ wrangle.workout.chart <- function(data = data) {
     distinct()
 
   return(data)
-
-}
-
-plot.workout.chart <- function(data = data, location = F) {
-
-  # plot chart of per-workout intensity over time
-
-  if (location == TRUE) {
-
-    plot <- ggplot(data, aes(x=date, y=index_intensity_bydatelocation, group=location, colour=location)) +
-      geom_line(aes(y=cummax_index_intensity_bydatelocation), size=1.5) +
-      scale_color_manual(values=c("#7570b3", "#1b9e77"))
-
-  } else {
-
-    plot <- ggplot(data, aes(x=date, y=index_intensity_bydate)) +
-      geom_line(aes(y=cummax_index_intensity_bydate), size=1.5) +
-      scale_color_manual(values=c("#1b9e77"))
-
-  }
-
-  plot <- plot +
-    geom_point(size=3, alpha=1/3) +
-    geom_line(stat="smooth",method = "lm", se=FALSE, size=1.5, alpha=1/3) +
-    ylim(0, NA) +
-    theme_minimal()
-
-  return(plot)
 
 }
