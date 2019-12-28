99a1409aedc2d0f228e1037f5cd7739af23b3b6f
Joel Becker <joel_becker@mail.com>
2019-12-28 13:51:15 +0000

    Refer to download function as from gym

--- tests/testthat/test-wrangle_intensity_metrics.R
+++ tests/testthat/test-wrangle_intensity_metrics.R
@@ -1,4 +1,4 @@
-data <- download_exercise_data(file_name = "exercise_data", file_format = "csv")
+data <- gym::download_exercise_data(file_name = "exercise_data", file_format = "csv")
 
 # TODO: test related to column names
 # TODO: test related to missing data
