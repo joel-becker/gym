8f9b1adf5cca13e0f4cd0aaef295f038b91b5797
Joel Becker <joel_becker@mail.com>
2019-12-28 13:50:07 +0000

    Add intensity metrics test file

---
+++ tests/testthat/test-wrangle_intensity_metrics.R
@@ -0,0 +1,5 @@
+data <- download_exercise_data(file_name = "exercise_data", file_format = "csv")
+
+# TODO: test related to column names
+# TODO: test related to missing data
+# TODO: test related to something else
