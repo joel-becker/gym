d41d324cc16501c4e295cec945e2f0d270794d21
Joel Becker <joel_becker@mail.com>
2019-12-28 14:49:53 +0000

    Make test explicit

--- tests/testthat/test-class_exercises.R
+++ tests/testthat/test-class_exercises.R
@@ -1,18 +1,10 @@
 # TODO: test download data function
 
-display.missing.exercise <- function(exercise_data, exercise_classes) {
-  # displays exercise which occur in inputted data but are not recognised by program
-  
-  input_exercises <- exercise_data[, "exercise"]
-  missing_exercises <- input_exercises[!(input_exercises %in% exercise_classes)]
-  
-  warning_message <- paste0(
-    "WARNING: charts will contain incorrectly grouped exercises, ",
-    "as the following exercises are not recognised by the program:",
-    "\t",
-    missing_exercises,
-    "\n"
-  )
-  
-  return(warning_message)
-}
+context("Testing data integrity")
+
+input_exercises <- exercise_data[, "exercise"]
+missing_exercises <- input_exercises[!(input_exercises %in% exercise_classes)]
+
+test_that("No exercises in data not contained in exercise_classes", {
+  expect_equal(length(missing_exercises), 0)
+})
