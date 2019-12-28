8b0264d0b27bf94ad29086b7c30f81fdde5b27ee
Joel Becker <joel_becker@mail.com>
2019-12-28 13:58:05 +0000

    Change tautology of colname test

--- tests/testthat/test-download_exercise_data.R
+++ tests/testthat/test-download_exercise_data.R
@@ -1,5 +1,5 @@
 data <- gym::download_exercise_data(file_name = "exercise_data", file_format = "csv")
-column_names <- colnames(data)
+column_names <- c("date", "exercise", "weight", "sets")
 
 test_that("Columns have correct names", {
   expect_named(data, column_names)
