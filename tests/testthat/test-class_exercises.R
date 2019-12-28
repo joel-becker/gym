08962cbad99fd72745506f9a3f8427ca6dcb6045
Joel Becker <joel_becker@mail.com>
2019-12-28 14:46:51 +0000

    Display missing exercise func

--- tests/testthat/test-class_exercises.R
+++ tests/testthat/test-class_exercises.R
@@ -1,1 +1,18 @@
 # TODO: test download data function
+
+display.missing.exercise <- function(exercise_data, exercise_classes) {
+  # displays exercise which occur in inputted data but are not recognised by program
+  
+  input_exercises <- exercise_data[, "exercise"]
+  missing_exercises <- input_exercises[!(input_exercises %in% exercise_classes)]
+  
+  warning_message <- paste0(
+    "WARNING: charts will contain incorrectly grouped exercises, ",
+    "as the following exercises are not recognised by the program:",
+    "\t",
+    missing_exercises,
+    "\n"
+  )
+  
+  return(warning_message)
+}
