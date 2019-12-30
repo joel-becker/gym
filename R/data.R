2c2b7d1058382cc07175c949cff181f3a6add829
Joel Becker <joel_becker@mail.com>
2019-12-30 17:40:54 +0000

    Describe remaining data

--- R/data.R
+++ R/data.R
@@ -8,3 +8,23 @@
 #'   \item{...}{Each other column is a binary variable indicating whether some exercise works some muscle.}
 #' }
 "exercise_classes"
+
+#' Aerobic excercises
+#'
+#' List of aerobic excercises
+#'
+#' @format A dataframe
+#' \describe{
+#'   \item{aerobic}{Name of aerobic excercise.}
+#' }
+"aerobic_excercises"
+
+#' Anaerobic excercises
+#'
+#' List of anaerobic excercises
+#'
+#' @format A dataframe
+#' \describe{
+#'   \item{anaerobic}{Name of anaerobic excercise.}
+#' }
+"anaerobic_excercises"
