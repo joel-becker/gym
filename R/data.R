3594a4dc6c41e28e1934445096bf27b7d273b6b2
Joel Becker <joel_becker@mail.com>
2019-12-30 17:38:16 +0000

    Describe ex classes

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Excercise classes
+#'
+#' Dataset relating exercises to muscle groups at various levels.
+#'
+#' @format A dataframe
+#' \describe{
+#'   \item{excercise_name}{Name of exercise.}
+#'   \item{...}{Each other column is a binary variable indicating whether some exercise works some muscle.}
+#' }
+"exercise_classes"
