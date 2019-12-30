ce39e992857e36adf551fc4afd5a8832d1641f90
Joel Becker <joel_becker@mail.com>
2019-12-30 00:02:16 +0000

    Impove style

--- R/download_exercise_data.R
+++ R/download_exercise_data.R
@@ -8,10 +8,13 @@
 #' @examples
 #' download_exercise_data()
 
-download_exercise_data <- function(file_name = "exercise_data", file_format = "csv") {
+download_exercise_data <- function(
+  file_name = "exercise_data",
+  file_format = "csv"
+  ) {
   # downloads and returns data from google drive
 
-  googledrive::drive_download(file_name, type = file_format, overwrite=TRUE)
+  googledrive::drive_download(file_name, type = file_format, overwrite = TRUE)
   path <- paste0(file_name, ".", file_format)
   data <- data.table::fread(path)
 
