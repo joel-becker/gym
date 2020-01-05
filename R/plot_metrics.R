7a17438e186d9a24fe66f7c93a7be4f0bc7735c4
Joel Becker <joel_becker@mail.com>
2020-01-05 17:16:21 -0500

    Correct aes() dependency

--- R/plot_metrics.R
+++ R/plot_metrics.R
@@ -9,11 +9,11 @@
 #' plot_time_chart()
 plot_time_chart <- function(data, location = F) {
   if (location == TRUE) {
-    plot <- ggplot2::ggplot(data, aes(x=date, y=decay_intensity_bydatelocation)) +
-      ggplot2::geom_line(stat="smooth", method = "lm", se=FALSE, aes(group=month_number), colour="#7570b3", size=1.2, alpha=0.3) +
+    plot <- ggplot2::ggplot(data, ggplot2::aes(x=date, y=decay_intensity_bydatelocation)) +
+      ggplot2::geom_line(stat="smooth", method = "lm", se=FALSE, ggplot2::aes(group=month_number), colour="#7570b3", size=1.2, alpha=0.3) +
       ggplot2::geom_line(size=1.2, colour="#7570b3")
   } else {
-    plot <- ggplot2::ggplot(data, aes(x=date, y=decay_intensity_bydate)) +
+    plot <- ggplot2::ggplot(data, ggplot2::aes(x=date, y=decay_intensity_bydate)) +
       ggplot2::geom_line(stat="smooth", method = "lm", se=FALSE, colour="#1b9e77", size=1.2, alpha=0.4) +
       ggplot2::geom_line(size=1.2, colour="#7570b3")
   }
@@ -38,14 +38,14 @@ plot_time_chart <- function(data, location = F) {
 plot_workout_chart <- function(data, location = F) {
   if (location == TRUE) {
     
-    plot <- ggplot2::ggplot(data, aes(x=date, y=index_intensity_bydatelocation, group=location, colour=location)) +
-      ggplot2::geom_line(aes(y=cummax_index_intensity_bydatelocation), size=1.5) +
+    plot <- ggplot2::ggplot(data, ggplot2::aes(x=date, y=index_intensity_bydatelocation, group=location, colour=location)) +
+      ggplot2::geom_line(ggplot2::aes(y=cummax_index_intensity_bydatelocation), size=1.5) +
       ggplot2::scale_color_manual(values=c("#7570b3", "#1b9e77"))
     
   } else {
     
-    plot <- ggplot2::ggplot(data, aes(x=date, y=index_intensity_bydate)) +
-      ggplot2::geom_line(aes(y=cummax_index_intensity_bydate), size=1.5) +
+    plot <- ggplot2::ggplot(data, ggplot2::aes(x=date, y=index_intensity_bydate)) +
+      ggplot2::geom_line(ggplot2::aes(y=cummax_index_intensity_bydate), size=1.5) +
       ggplot2::scale_color_manual(values=c("#1b9e77"))
     
   }
