313b4d213a07d15ea439953e97396514f23bb2d4
Joel Becker <joel_becker@mail.com>
2020-01-05 17:08:18 -0500

    Plot workout chart

--- R/plot_metrics.R
+++ R/plot_metrics.R
@@ -26,3 +26,35 @@ plot_time_chart <- function(data, location = F) {
 }
 
 
+#' @title Plot workout chart
+#'
+#' @description Plot chart of per-workout intensity over time
+#' @param data Data from wrangle_workout_chart()
+#' @param location Splitting groups by location, FALSE by default
+#' @keywords plot
+#' @export
+#' @examples
+#' plot_workout_chart()
+plot_workout_chart <- function(data, location = F) {
+  if (location == TRUE) {
+    
+    plot <- ggplot2::ggplot(data, aes(x=date, y=index_intensity_bydatelocation, group=location, colour=location)) +
+      ggplot2::geom_line(aes(y=cummax_index_intensity_bydatelocation), size=1.5) +
+      ggplot2::scale_color_manual(values=c("#7570b3", "#1b9e77"))
+    
+  } else {
+    
+    plot <- ggplot2::ggplot(data, aes(x=date, y=index_intensity_bydate)) +
+      ggplot2::geom_line(aes(y=cummax_index_intensity_bydate), size=1.5) +
+      ggplot2::scale_color_manual(values=c("#1b9e77"))
+    
+  }
+  
+  plot <- plot +
+    ggplot2::geom_point(size=3, alpha=1/3) +
+    ggplot2::geom_line(stat="smooth",method = "lm", se=FALSE, size=1.5, alpha=1/3) +
+    ggplot2::ylim(0, NA) +
+    ggplot2::theme_minimal()
+  
+  return(plot)
+}
